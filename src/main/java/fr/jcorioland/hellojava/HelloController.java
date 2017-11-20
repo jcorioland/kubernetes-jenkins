451307284243c88a5f5d9a25659f3cca3a95dcb2
Julien Corioland <julien.corioland@live.fr>
2017-11-20 10:37:28 +0100

    basic hello api + pom.xml

---
+++ src/main/java/fr/jcorioland/hellojava/HelloController.java
@@ -0,0 +1,19 @@
+package fr.jcorioland.hellojava;
+
+import java.net.InetAddress;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        String hostname = "Unknown";
+        try {
+            hostname = InetAddress.getLocalHost().getHostName();
+        } catch (Exception e) {
+            // nothing.
+        }
+        return "Hello World !! I am running on " + hostname;
+    }
+}
---
+++ src/main/java/fr/jcorioland/hellojava/HelloJavaApplication.java
@@ -0,0 +1,12 @@
+package fr.jcorioland.hellojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJavaApplication.class, args);
+	}
+}
